c4dbc9456e437888841112042210fb8f4ae26bbd
Ramazan Yol <rmznyol@gmail.com>
2023-11-06 12:15:22 -0800

    update abstractFetcher to call data in script

--- src/abstractFetcher.py
+++ src/abstractFetcher.py
@@ -2,59 +2,68 @@ import arxiv
 import pandas as pd
 from tqdm import tqdm
 
-# Define your search query
-while True:
-    query = input("search (e.g 'Minimal Surfaces'): ")
-    if query:
-        break
 
-# Set the number of results you want to retrieve
-while True:
-    try:
-        max_results = int(input("Maximum number of results requested: "))
-    except ValueError:
-        print("Please enter a positive integer number")
-        continue
-    else:
-        break
+def fetch_and_save(query:str, max_results:int):
+    #check if it is a valid query
+    query_check(query, max_results)
 
-# Create an empty DataFrame to store the data
-df = pd.DataFrame(
-    columns=[
-        "Title",
-        "Authors",
-        "Published",
-        "Abstract",
-        "Link",
-        "PrimaryCategory",
-        "Categories",
-    ]
-)
-
-# Search for articles on arXiv
-search = arxiv.Search(
+    # Search for articles on arXiv
+    search = arxiv.Search(
     query=query, max_results=max_results, sort_by=arxiv.SortCriterion.Relevance
-)
-
-# Iterate through the search results and append data to the DataFrame
-iterator_with_tqdm = tqdm(search.results(), total=max_results)
-for result in iterator_with_tqdm:
-    iterator_with_tqdm.set_description(f"Loading {result.title[:20]} ...")
-    article_info = {
-        "Title": result.title,
-        "Authors": ", ".join([author.name for author in result.authors]),
-        "Published": result.published,
-        "Abstract": result.summary,
-        "Link": result.pdf_url,
-        "PrimaryCategory": result.primary_category,
-        "Categories": result.categories,
-    }
-    df_article = pd.DataFrame([article_info])
-    df = pd.concat(
-        [df_iter for df_iter in [df, df_article] if not df_iter.empty],
-        ignore_index=True,
     )
 
-# Save the DataFrame to a CSV file
-file_path = f'data/{query.strip().replace(" ", "_").lower()}_articles.csv'
-df.to_csv(file_path, index=False)
+    #save your data as a dataframe
+    file_path_to_save = f'data/{query.strip().replace(" ", "_").lower()}_articles.csv'
+    save_data(search, file_path_to_save, max_results)
+
+    return file_path_to_save
+# Define your search query
+def query_check(query:str ,max_results: int):
+    if not query:
+        raise ValueError('empty query')
+    if type(query) != str:
+        raise TypeError('please enter a string of query')
+    if not max_results != int:
+        raise TypeError('Please enter a positive integer number')
+    if max_results < 0:
+        raise ValueError('please enter a positive integer')
+    return query, max_results
+
+def save_data(search, file_path_to_save, max_results):
+    # Create an empty DataFrame to store the data
+    df = pd.DataFrame(
+        columns=[
+            "Title",
+            "Authors",
+            "Published",
+            "Abstract",
+            "Link",
+            "PrimaryCategory",
+            "Categories",
+        ]
+    )
+
+    # Iterate through the search results and append data to the DataFrame
+    iterator_with_tqdm = tqdm(search.results(), total=max_results)
+    for result in iterator_with_tqdm:
+        iterator_with_tqdm.set_description(f"Saving {result.title[:20]} ...")
+        article_info = {
+            "Title": result.title,
+            "Authors": ", ".join([author.name for author in result.authors]),
+            "Published": result.published,
+            "Abstract": result.summary,
+            "Link": result.pdf_url,
+            "PrimaryCategory": result.primary_category,
+            "Categories": result.categories,
+        }
+        df_article = pd.DataFrame([article_info])
+        df = pd.concat(
+            [df_iter for df_iter in [df, df_article] if not df_iter.empty],
+            ignore_index=True,
+        )
+    # Save the DataFrame to a CSV file
+    df.to_csv(file_path_to_save, index=False)
+
+if __name__ == '__main__':
+    # get data
+    fetch_and_save('Minimal Surfaces', 20)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+from abstractFetcher import fetch_and_save
+from vectorizer import vectorize_and_save
 import pandas as pd 
+import joblib
 
-df = pd.read_csv('data/minimal_surfaces_articles.csv')
+query = 'Minimal Surfaces'
+max_results = 20 
 
-print(df['Abstract'].iloc[1])
+# # if query is already run, skip this step 
+# unprocessed_data_file_path = fetch_and_save(query, max_results)
+
+# print(unprocessed_data_file_path)
+
+# column_to_vectorize = 'Abstract'
+# vectorized_data_file_path = vectorize_and_save(unprocessed_data_file_path,
+#                                                column_to_vectorize)
+
+
+vectorized_data_file_path = 'data/Abstract_tfidf_sparse_matrix.pkl'
+tfidf_sparse_matrix = joblib.load(vectorized_data_file_path)
+
+print(tfidf_sparse_matrix.shape)
+
+vectors_df = pd.DataFrame(tfidf_sparse_matrix.toarray())
+print(vectors_df.head(10))
